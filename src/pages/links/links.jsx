07d5c79a8bb55e55851bc4a215ca7a099bf0147a
RogerTerrazas <36799381+RogerTerrazas@users.noreply.github.com>
2019-06-28 18:58:22 -0500

    links page

--- src/pages/links/links.jsx
+++ src/pages/links/links.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
-function links() {
+const useStyles = makeStyles(theme => ({
+	button: {
+		padding: '2%',
+		marginBottom: '1.5%',
+	},
+	container: {
+		margin: '5%',
+	},
+}));
+
+export default function Links() {
+	const classes = useStyles();
+
 	return (
-		<div >
-			<header >
-
-				<p>
-					Eweek Links page
-				</p>
-			</header>
+		<div className={classes.container}>
+			<Grid container spacing={12} >
+				<Grid item xs={12}>
+					<Button href="https://sec.engr.utexas.edu/" fullWidth variant="contained" color="primary" className={classes.button}>
+							UT Student Engineering Council
+					</Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Button href="https://www.facebook.com/SECtexas/" fullWidth variant="contained" color="primary" className={classes.button}>
+						UT Student Engineering Council Facebook
+					</Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Button href="https://github.com/eweekweb/" fullWidth variant="contained" color="primary" className={classes.button}>
+						EWeek GitHub Repo
+					</Button>
+				</Grid>
+			</Grid>
 		</div>
+
 	);
 }
-
-export default links;
