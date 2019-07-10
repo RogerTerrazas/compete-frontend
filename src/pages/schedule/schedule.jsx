88a3de2024188e06d8f97f14fe2edf539c9b78ea
RogerTerrazas <36799381+RogerTerrazas@users.noreply.github.com>
2019-07-10 14:55:10 -0500

    Implemented the schedule with my own email for the time being. I was logged out of the eweek gmail so once I am able to get the embedded code, I will switch it out. I branched off of the planning calendar because I used the same style sheet the planning calendar used.

--- src/pages/schedule/schedule.jsx
+++ src/pages/schedule/schedule.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React from "react";
+import _navbar from "../../components/_navbar";
+import { Footer } from "../home/home";
+import '../../css/planningCalendar.css';
 
-function schedule() {
-	return (
-		<div >
-			<header >
-
-				<p>
-					Eweek Schedule page
-				</p>
-			</header>
-		</div>
-	);
+class schedule extends React.Component{
+	render(){
+		return(
+			<div className='planning-calendar'>
+				<_navbar/>
+				<div className='container'>
+					<div className='header'>
+						<h1>E-Week Calendar</h1>
+						<p></p>
+					</div>
+					<div className='calendar'>
+						<iframe src="https://calendar.google.com/calendar/embed?src=roger.terrazas%40utexas.edu&ctz=America%2FChicago" style={{width:'100%', height:'100vh'}}/>
+					</div>
+				</div>
+				<Footer/>
+			</div>
+		)
+	}
 }
 
 export default schedule;
