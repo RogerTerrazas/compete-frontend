eb047c60e478f51f48c1df75ca6147de09d162f1
Danika Widjaja <danika.dew@utexas.edu>
2019-07-29 20:40:40 -0500

    attendance form airtable integration

---
+++ src/pages/forms/attendanceForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class attendanceForm extends React.Component{
+    render(){
+        return(
+            <div>
+                <iframe class="airtable-embed" src="https://airtable.com/embed/shrhknyzUiX1QYirf?backgroundColor=teal" frameborder="0" onmousewheel="" style={{width:"100%", height:"200vh", background: "transparent", border: "1px solid #ccc"}}></iframe>
+            </div>
+        )
+    }
+}
+
+export default attendanceForm
